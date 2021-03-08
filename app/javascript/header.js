c5afc4d676acddbbd40fb0351728fe6a60a435d0
tsuyoki1124 <tsuyoki.tanokura@gmail.com>
2021-03-08 23:26:38 +0900

    pulldown表示の条件分岐

--- app/javascript/header.js
+++ app/javascript/header.js
@@ -2,14 +2,14 @@ window.addEventListener('load', function(){
   const pullDownButton = document.getElementById("worklists")
   const pullDownParents = document.getElementById("pull-down")
 
-  pullDownButton.addEventListener('mouseover', function(){
-    console.log("mouseover OK")
-    pullDownParents.setAttribute("style", "display:block;")
-  })
-  
-  pullDownButton.addEventListener('mouseout', function(){
-    console.log("mouseout OK")
-    pullDownParents.removeAttribute("style", "display:block;")
+  pullDownButton.addEventListener('click', function() {
+    
+    // プルダウンメニューの表示と非表示の設定
+    if (pullDownParents.getAttribute("style") == "display:block;") {
+      pullDownParents.removeAttribute("style", "display:block;")
+    } else {
+      pullDownParents.setAttribute("style", "display:block;")
+    }
   })
   
 })
