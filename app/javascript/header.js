da3b0c55d8ef0444fbeedff6cbf86198c51cacb3
tsuyoki1124 <tsuyoki.tanokura@gmail.com>
2021-03-08 22:37:14 +0900

    pulldownの表示

--- app/javascript/header.js
+++ app/javascript/header.js
@@ -1,2 +1,15 @@
-const pullDownButton = document.getElementById("worklists")
-console.log(pullDownButton)
+window.addEventListener('load', function(){
+  const pullDownButton = document.getElementById("worklists")
+  const pullDownParents = document.getElementById("pull-down")
+
+  pullDownButton.addEventListener('mouseover', function(){
+    console.log("mouseover OK")
+    pullDownParents.setAttribute("style", "display:block;")
+  })
+  
+  pullDownButton.addEventListener('mouseout', function(){
+    console.log("mouseout OK")
+    pullDownParents.removeAttribute("style", "display:block;")
+  })
+  
+})
