813ba77fda43d1e271371092a8d23462d92f0cb4
tsuyoki1124 <tsuyoki.tanokura@gmail.com>
2021-03-12 19:22:35 +0900

    travel indexへの遷移実装

---
+++ app/controllers/travels_controller.rb
@@ -0,0 +1,4 @@
+class TravelsController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
 root to: 'boards#index'
+resources :travels, only: :index
 end
